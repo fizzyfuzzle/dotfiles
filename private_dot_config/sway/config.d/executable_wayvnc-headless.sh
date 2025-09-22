fe113758103c4e5c5cbf3db89614a847f1954636
Marco <39180766+fizzyfuzzle@users.noreply.github.com>
2025-09-22 17:49:04 +0200

    Add .config/sway/config.d/wayvnc-headless.sh

---
+++ private_dot_config/sway/config.d/executable_wayvnc-headless.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#
+HEADLESS="HEADLESS-1"
+OUTPUTS_TO_RECONNECT=()
+
+restore_outputs() {
+    [[ ${#OUTPUTS_TO_RECONNECT[@]} -ge 1 ]] || return
+    for output in "${OUTPUTS_TO_RECONNECT[@]}"; do
+        echo "Re-enabling output $output"
+        swaymsg output "$output" enable
+    done
+    echo "Disabling virtual output $HEADLESS"
+    swaymsg output "$HEADLESS" disable
+    OUTPUTS_TO_RECONNECT=()
+}
+trap restore_outputs EXIT
+
+collapse_outputs() {
+    echo "Switching to preexisting virtual output $HEADLESS"
+    swaymsg output "$HEADLESS" enable
+    wayvncctl output-set "$HEADLESS"
+    for output in $(wayvncctl -j output-list | jq -r '.[] | select(.captured==false).name'); do
+        echo "Disabling extra output $output"
+        swaymsg output "$output" disable
+        OUTPUTS_TO_RECONNECT+=("$output")
+    done
+}
+
+connection_count_now() {
+    local count=$1
+    if [[ $count == 1 ]]; then
+        collapse_outputs
+    elif [[ $count == 0 ]]; then
+        restore_outputs
+    fi
+}
+
+# Start WayVNC Controller
+while IFS= read -r EVT; do
+    case "$(jq -r '.method' <<<"$EVT")" in
+        client-*onnected)
+            count=$(jq -r '.params.connection_count' <<<"$EVT")
+            connection_count_now "$count"
+            ;;
+        wayvnc-shutdown)
+            echo "wayvncctl is no longer running"
+            connection_count_now 0
+            ;;
+        wayvnc-startup)
+            echo "Ready to receive wayvnc events"
+            ;;
+    esac
+done < <(wayvncctl --wait --reconnect --json event-receive)
