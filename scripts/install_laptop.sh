9451207eb3ab1113f0d10e7f1ebc001df1d454f5
Marco <39180766+fizzyfuzzle@users.noreply.github.com>
2025-09-26 17:04:35 +0200

    Updated laptop install script

---
+++ scripts/install_laptop.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+# Do not install recommended packages
+run0 sed -i 's/^#\?Recommends=.*/Recommends=false/' /etc/rpm-ostreed.conf
+
+# Add Brave Repository
+run0 curl --tlsv1.3 -fsSLo /etc/yum.repos.d/brave-browser.repo \
+    https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+
+# Overlay Additional Packages
+rpm-ostree install --idempotent --asumeyes \
+    brave-browser \
+    neovim \
+    wayvnc \
+    wireguard-tools \
+    zsh
+
+# Disable System Services
+systemctl mask \
+    bluetooth.service \
+    cups.service cups.socket cups.path \
+    ModemManager.service \
+    sddm.service
+
+# Disable User Services
+systemctl --user mask \
+    app-blueman@autostart.service \
+    app-geoclue\x2ddemo\x2dagent@autostart.service \
+    blueman-applet.service \
+    blueman-manager.service \
+    mpris-proxy.service \
+    obex.service
+
+# Update GRUB timeout
+echo "set timeout=0" | run0 tee /boot/grub2/user.cfg
+
+# Reboot (Only needed on first run)
+command -v zsh &>/dev/null || systemctl reboot
+
+# Change Shell to ZSH
+chsh --shell=/usr/bin/zsh
+
+# Add Flathub
+flatpak remote-add --user --if-not-exists \
+    flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+# Install Flatpaks
+flatpak install --user --assumeyes \
+    com.github.tchx84.Flatseal \
+    org.keepassxc.KeePassXC \
+    org.libreoffice.LibreOffice \
+    org.qbittorrent.qBittorrent \
+    com.visualstudio.code
+
