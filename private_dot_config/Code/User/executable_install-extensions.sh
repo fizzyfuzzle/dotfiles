605fc48c08532f6dfc1d68f38c3dd3de87082d58
Marco <39180766+fizzyfuzzle@users.noreply.github.com>
2025-09-29 15:18:05 +0200

    Update .config/Code/User/install-extensions.sh Update .config/Code/User/settings.json

--- private_dot_config/Code/User/executable_install-extensions.sh
+++ private_dot_config/Code/User/executable_install-extensions.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
 #
-# Install VSCode Extensions
-#
+CODE="flatpak run com.visualstudio.code"
 EXTENSIONS=(
   hashicorp.terraform \
   ms-python.isort \
   ms-python.flake8 \
   ms-python.python \
   ms-vscode-remote.remote-ssh \
-  pkief.material-icon-theme \
   redhat.ansible
 )
 
-# Check if CODE is installed
-command -v code &>/dev/null || {
-  echo "VSCODE NOT INSTALLED ..."
-  exit 1
-}
-
 # Install Extensions
 for E in "${EXTENSIONS[@]}"; do
-  code --install-extension "${E}"
+  "${CODE}" --install-extension "${E}"
 done
