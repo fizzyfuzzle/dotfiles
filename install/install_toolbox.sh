1c7935ffbe8a818c6a008a5c05cdc571ba766c04
Marco <39180766+fizzyfuzzle@users.noreply.github.com>
2025-09-29 14:09:16 +0200

    Updated toolbox install script

---
+++ install/install_toolbox.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Do not install recommended packages
+echo "[main]\ninstall_weak_deps=False" | sudo tee /etc/dnf/dnf.conf
+
+# Install Packages
+sudo dnf install --assumeyes \
+    age \
+    chezmoi \
+    cryptsetup \
+    mkfs.ext4 \
+    qrencode \
+    steghide \
+    wl-clipboard \
+    zsh
