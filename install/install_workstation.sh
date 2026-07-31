8cb3700717fe696333cc9b2d3bed735c801d31be
Marco <nobody@home.now>
2026-07-31 18:47:51 +0200

    Updates

--- install/install_workstation.sh
+++ install/install_workstation.sh
@@ -49,7 +49,6 @@ systemctl --user mask \
     app-nm\\x2dapplet@autostart.service \
     blueman-applet.service \
     blueman-manager.service \
-    gnome-keyring-daemon.service gnome-keyring-daemon.socket \
     mpris-proxy.service \
     obex.service
 
