9e386d6df9ee3a393454cd8ea3bb37bb873adbc4
Marco <nobody@home.now>
2026-08-01 13:41:28 +0200

    Update .local/bin/toolbox-refresh.sh

--- dot_local/bin/executable_toolbox-refresh.sh
+++ dot_local/bin/executable_toolbox-refresh.sh
@@ -7,5 +7,5 @@ container="fedora-toolbox-$(rpm -E %fedora)"
 podman container exists "$container" && toolbox rm "$container"
 
 toolbox create --assumeyes
-toolbox run sudo dnf install --setopt install_weak_deps=false --refresh --assumeyes \
+toolbox run sudo dnf install --setopt install_weak_deps=false --refresh --assumeyes --quiet \
     ansible offlineimap opentofu pcsc-lite-libs python3-dateutil python3-requests qrencode steghide zsh
