e1a6f98b82b879ef31c1d9665b64f0e8fb797333
Marco <nobody@home.now>
2026-08-01 13:28:42 +0200

    Add .local/bin/toolbox-refresh.sh

---
+++ dot_local/bin/executable_toolbox-refresh.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# vim: filetype=sh
+set -euo pipefail
+
+container="fedora-toolbox-$(rpm -E %fedora)"
+
+podman container exists "$container" && toolbox rm "$container"
+
+toolbox create --assumeyes
+toolbox run sudo dnf install --setopt install_weak_deps=false --refresh --assumeyes \
+    ansible offlineimap opentofu pcsc-lite-libs python3-dateutil python3-requests qrencode steghide zsh
