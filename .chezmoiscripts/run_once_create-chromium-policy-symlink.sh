c2fb3637216c8945ca40290a3b24627ebf7811ac
Marco <nobody@home.now>
2026-08-03 14:01:14 +0200

    Updates

--- .chezmoiscripts/run_once_create-chromium-policy-symlink.sh
+++ .chezmoiscripts/run_once_create-chromium-policy-symlink.sh
@@ -1,6 +1,9 @@
-#!/bin/bash
+{{- if .workstation -}}
+#!/usr/bin/env bash
+# vim: filetype=sh
 set -euo pipefail
 
 EXT_DIR="${XDG_DATA_HOME:-$HOME/.local/share}/flatpak/extension/io.github.ungoogled_software.ungoogled_chromium.Policy.local/$(uname -m)"
 mkdir -p "${EXT_DIR}"
 ln -sfn "$HOME/.local/share/chromium-policy" "${EXT_DIR}/1"
+{{- end -}}
