66a0af39d5ae9439c6e21df005c9f7d2b27d6215
Marco <nobody@home.now>
2026-08-07 11:54:03 +0200

    Update .var/app/com.visualstudio.code/config/Code/User/install-extensions.sh

--- dot_var/app/com.visualstudio.code/config/private_Code/User/executable_install-extensions.sh
+++ dot_var/app/com.visualstudio.code/config/private_Code/User/executable_install-extensions.sh
@@ -2,6 +2,7 @@
 #
 EXTENSIONS=(
   ms-python.python \
+  ms-pyright.pyright \
   charliermarsh.ruff \
   opentofu.vscode-opentofu \
   redhat.ansible \
@@ -12,3 +13,14 @@ EXTENSIONS=(
 for E in "${EXTENSIONS[@]}"; do
   flatpak run com.visualstudio.code --install-extension "${E}"
 done
+
+EXTENSIONS=(
+  ms-python.vscode-pylance \
+  ms-python.debugpy
+)
+
+# Remove Extensions
+for E in "${EXTENSIONS[@]}"; do
+  flatpak run com.visualstudio.code --uninstall-extension "${E}" 2>/dev/null || true
+done
+
